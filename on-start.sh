1320e24bb87f0a964ec6fa020231ff28cd29ce98
Manoj <manojlds@gmail.com>
2020-09-28 03:27:44 +0530

    Fix crontab script directory

--- on-start.sh
+++ on-start.sh
@@ -10,6 +10,6 @@ echo "Setting up cron with the auto stop script"
 
 
 if ! (crontab -l 2>/dev/null | grep "auto_stop_idle.py"); then
-    (crontab -l 2>/dev/null; echo -e "SAGEMAKER_JUPYER_IDLE_TIME=60\nSAGEMAKER_JUPYTER_PORT=8443\nSAGEMAKER_JUPYTER_SSL=true\n*/5 * * * * '$DIR/auto_stop_idle.py | tee -a /home/ec2-user/SageMaker/auto_stop_idle.log'") | crontab -
+    (crontab -l 2>/dev/null; echo -e "SAGEMAKER_JUPYER_IDLE_TIME=60\nSAGEMAKER_JUPYTER_PORT=8443\nSAGEMAKER_JUPYTER_SSL=true\n*/5 * * * * '$PWD/auto_stop_idle.py | tee -a /home/ec2-user/SageMaker/auto_stop_idle.log'") | crontab -
 fi
 
